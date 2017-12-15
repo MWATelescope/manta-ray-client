cef8a9205aeb5fabc364edfa4d77c1946f054904
davepallot <dave.pallot@uwa.edu.au>
2017-12-15 14:33:50 +0800

    Use find_packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='mantaray-client',
       version='0.1.0',
-      packages=['mantaray'],
+      packages=find_packages(),
       install_requires=['requests',
                         'websocket_client',
                         'colorama'],
