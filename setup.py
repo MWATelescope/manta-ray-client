e10ad8e53ccdacff36a51d0311714cb15d3068c7
Mouriyan-Rajendran <mouriyan.rajendran@gmail.com>
2025-05-06 14:15:33 +0800

    updating the revision

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mantaray-client",
-    version="2.0.0",
+    version="2.0.1",
     packages=find_packages(),
     install_requires=["requests>=2.18.3", "websocket_client", "colorama"],
     entry_points={
